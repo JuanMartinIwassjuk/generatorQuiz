75709c7f799bb1cc12d5cc748a773a940ec3af2a
Juan Martin <jiwassjuk@frba.utn.edu.com.ar>
2024-03-08 09:09:30 -0300

    generator

---
+++ main.py
@@ -0,0 +1,29 @@
+import openai
+import json
+from config import API_KEY, MODEL_NAME
+def get_openai_response_in_json_format(number_of_questions, number_of_options, difficulty_level, topic):
+    
+    openai.api_key = API_KEY
+
+    model = MODEL_NAME
+
+    prompt = f"generate {number_of_questions} specific questions with a difficulty level of {difficulty_level} about the topic {topic}"
+
+    messages = [
+        {'role': 'system', 'content': f'have {number_of_options} options for each question, including the correct answer, and your response in JSON format'},
+        {'role': 'user', 'content': prompt}
+    ]
+
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0.4 # Nivel de creatividad, el más alto es 1 que podría llegar a ser menos preciso
+    )
+
+    json_response = response["choices"][0]["message"]["content"]
+
+
+
+    return json_response
+
+    
---
+++ quiz.py
@@ -0,0 +1,26 @@
+import main
+from config import NUMBER_OF_QUESTIONS,NUMBER_OF_OPTIONS,LEVEL_OF_DIFFICULTY,TOPIC
+class Quiz:
+    def __init__(self, questions):
+        self.questions = questions
+
+    def display_question(self, question_number):
+        if 0 <= question_number < len(self.questions):
+            question_data = self.questions[question_number]
+            print(question_data["question"]) # Esta es una pregunta
+            for option in question_data["options"]:
+                print(option) # Estas son las diferentes opciones de la pregunta
+            correct_answer = question_data["correct_answer"] # opcion correcta de la pregunta
+            print(f"Correct Answer: {correct_answer}\n")
+        else:
+            print("Invalid question number.")
+
+# Devuelve la respuesta de la ia en formato json
+quiz_data = main.get_openai_response_in_json_format(NUMBER_OF_QUESTIONS,NUMBER_OF_OPTIONS, LEVEL_OF_DIFFICULTY, TOPIC)
+
+# crea el Quiz
+quiz = Quiz(quiz_data["questions"])
+
+# Display all questions and options with correct answers
+for i in range(len(quiz.questions)):
+    quiz.display_question(i)
