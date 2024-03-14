cafd7e2fa9990fd588b406873ec63969f4eb1985
Juan Martin <jiwassjuk@frba.utn.edu.com.ar>
2024-03-14 16:33:17 -0300

    Disparador ok

--- disparador.py
+++ disparador.py
@@ -4,6 +4,7 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import os
 import sys
+from config import NUMBER_OF_QUESTIONS
 
 directory_to_watch = os.getcwd()+'/audio'
 script_to_run = 'python generatorVideo.py'
@@ -14,8 +15,8 @@ files_created = 0
 # Función para ejecutar el script cuando se creen suficientes archivos
 def run_script():
     global files_created
-    if files_created >= 2:
-        print("Se han creado 6 archivos. Ejecutando el script...")
+    if files_created >= NUMBER_OF_QUESTIONS:
+        print(f"Se han creado {NUMBER_OF_QUESTIONS} audios de voz. Ejecutando el script...")
         time.sleep(8)
         os.system(script_to_run)
         print("ejecutado correctamente, 'ctrl+c' para finalizar")
